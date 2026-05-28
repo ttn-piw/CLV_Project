64300a14d83b6cd69dfaeddddf35cc152ed44e6d
ttn-piw <ttrungnguyen2003@gmail.com>
2026-05-28 15:52:12 +0700

    Define standard API response

--- backend/src/authors/authors.controller.ts
+++ backend/src/authors/authors.controller.ts
@@ -6,14 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  Logger,
 } from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { ApiResponse } from 'src/common/dto/api-response';
 
 @Controller('authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
+  private readonly logger = new Logger(AuthorsController.name);
 
   @Post()
   create(@Body() createAuthorDto: CreateAuthorDto) {
@@ -21,8 +24,13 @@ export class AuthorsController {
   }
 
   @Get()
-  findAll() {
-    return this.authorsService.findAll();
+  async findAll() {
+    const authors = await this.authorsService.findAll();
+    this.logger.log(authors);
+    const code = authors !== null ? 200 : 500;
+    const message = code === 200 ? 'Successfully!' : 'Something failed!';
+
+    return new ApiResponse(code, message, authors);
   }
 
   @Get(':id')
