068ff8ec54deaf4047a7af7290ef61dd2fffeeca
ttn-piw <ttrungnguyen2003@gmail.com>
2026-05-28 14:08:30 +0700

    Define database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASS'),
+        database: configService.get('DB_DBName'),
+        entities: [join(process.cwd(), 'dist/**/*.entity.js')],
+        synchronize: true, // not use in PRODUCTION
+      }),
+    }),
+    AuthorsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
